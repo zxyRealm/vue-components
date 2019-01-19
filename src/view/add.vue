83a5a11dc9f94e77d2cd296f899a8c0de12c8513
折威 <zhangxiaoyuan@uni-ubi.com>
2019-01-19 12:52:44 +0800

    提交测试

---
+++ src/view/add.vue
@@ -0,0 +1,25 @@
+<template>
+  <h3>
+    this is add test
+  </h3>
+</template>
+
+<script>
+export default {
+  name: 'add',
+  data () {
+    return {
+    }
+  },
+  created () {},
+  mounted () {},
+  computed: {},
+  methods: {
+  },
+  watch: {}
+}
+</script>
+
+<style scoped>
+
+</style>
