119391e6de420b12ae3236cbe0ddb18eb83bf2cc
折威 <zhangxiaoyuan@uni-ubi.com>
2019-01-19 13:47:12 +0800

    忽略提交某个已修改的文件

---
+++ src/view/demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    这个文件是要提交的！！！！
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'demo',
+  data () {
+    return {
+    }
+  },
+  created () {},
+  mounted () {},
+  computed: {},
+  methods: {
+  },
+  watch: {}
+}
+</script>
+
+<style scoped>
+
+</style>
