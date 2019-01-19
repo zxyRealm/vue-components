0c6f609430800038b34a319967c6820f1d233f93
折威 <zhangxiaoyuan@uni-ubi.com>
2019-01-19 13:01:21 +0800

    提交上次修改

---
+++ src/view/add.vue
@@ -0,0 +1,26 @@
+<template>
+  <h3>
+    this is add test
+  </h3>
+  <p>这次我又改了，但是不想提交，能取消吗？</p>
+</template>
+
+<script>
+export default {
+  name: 'add',
+  data () {
+    return {
+    }
+  },
+  created () {},
+  mounted () {},
+  computed: {},
+  methods: {
+  },
+  watch: {}
+}
+</script>
+
+<style scoped>
+
+</style>
